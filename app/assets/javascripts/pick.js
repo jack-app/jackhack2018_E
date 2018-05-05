c816539da0a4781882c1d21c6ab429dfc99bdd04
nakata yuki <nasutaro211@gmail.com>
2018-05-05 13:20:46 +0900

    一旦完成！

---
+++ app/assets/javascripts/pick.js
@@ -0,0 +1,13 @@
+// var bin;
+//
+//
+// function init(){
+//   bin =     bin = document.getElementById("pyonBin");
+//
+// }
+//
+// function pyonPyon(height, direction){
+//   if bin.
+//   height = height*0.3;
+//
+// }
